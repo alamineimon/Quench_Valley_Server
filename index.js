8ba9751e3864b2013b0040b4b8a2a02729826b06
eimonalamin <eimonalamin@gmail.com>
2022-12-05 08:12:16 -0800

    get all service

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Quench_Valley
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
@@ -8,6 +9,30 @@ require("dotenv").config();
 //mmiddle wares
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.juguzvf.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+  try{
+    const serviceCollection = client.db("Quench_Valley").collection("services");
+
+     //for get all service
+     app.get("/services", async (req, res) => {
+      const query = {};
+      const result =await serviceCollection.find(query).toArray()
+      res.send(result);
+    });
+
+  }
+  finally{
+
+  }
+}run().catch((err) => console.error(err));
+
 
 
 app.get("/", (res, req) => {
